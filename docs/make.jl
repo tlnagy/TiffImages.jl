f6418829caef0ea5f6692861cc4e16a4eeeb33d3
Tamas Nagy <github@tamasnagy.com>
2020-03-18 15:06:06 -0700

    finish doc setup

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,9 @@
 using Documenter
 using TIFF
 
+DocMeta.setdocmeta!(TIFF, :DocTestSetup, :(using TIFF); recursive=true)
 makedocs(modules=[TIFF], sitename="TIFF.jl")
+
+deploydocs(
+    repo = "github.com/tlnagy/TIFF.jl.git",
+)
