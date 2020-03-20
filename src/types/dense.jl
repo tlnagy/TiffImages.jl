85893eb6587fe84723740bc592a6067733a2f9f6
Tamas Nagy <github@tamasnagy.com>
2020-03-20 14:39:03 -0700

    forward setindex commands to backing array

--- src/types/dense.jl
+++ src/types/dense.jl
@@ -13,6 +13,8 @@ Base.size(t::DenseTaggedImage) = size(t.data)
 Base.getindex(img::DenseTaggedImage, i...) = getindex(img.data, i...)
 Base.getindex(img::DenseTaggedImage{T, 3}, i::Int, j::Int, k::Int) where {T} = getindex(img.data, i, j, k)
 
+Base.setindex!(img::DenseTaggedImage, i...) = setindex!(img.data, i...)
+
 function Base.getindex(img::DenseTaggedImage{T, 3}, i::Colon, j::Colon, k) where {T}
     DenseTaggedImage(getindex(img.data, i, j, k), img.ifds[k])
 end
