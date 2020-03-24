fe43936cb53c995cd90180b77b2ea1b436d3cc58
Tamas Nagy <github@tamasnagy.com>
2020-03-24 12:30:07 -0700

    parameterize on backing array's type for speed
    This has a pretty massive effect on performance. Accessing a single
    pixel went from ~20ns to 8ns with this change. Only 5x slower than no
    lazy transformation at all. Working on #4

--- src/types/dense.jl
+++ src/types/dense.jl
@@ -1,17 +1,28 @@
-struct DenseTaggedImage{T, N, O} <: AbstractDenseTIFF{T, N} where {O <: Unsigned}
-    data::AbstractArray{T, N}
+using Base: @propagate_inbounds
 
+struct DenseTaggedImage{T, N, O <: Unsigned,AA <: AbstractArray} <: AbstractDenseTIFF{T, N}
+    data::AA
     ifds::Vector{IFD{O}}
+
+    function DenseTaggedImage(data::AbstractArray{T, N}, ifds::Vector{IFD{O}}) where {T, N, O}
+        if N == 3
+            @assert size(data, 3) == length(ifds)
+        elseif N == 2
+            @assert length(ifds) == 1
+        end
+        new{T, N, O, typeof(data)}(data, ifds)
+    end
 end
 
 function DenseTaggedImage(data::AbstractArray{T, 2}, ifd::IFD{O}) where {T, O}
-    DenseTaggedImage(data, IFD{O}[ifd])
+    DenseTaggedImage{T, 2}(data, IFD{O}[ifd])
 end
 
 Base.size(t::DenseTaggedImage) = size(t.data)
 
-Base.getindex(img::DenseTaggedImage, i...) = getindex(img.data, i...)
-Base.getindex(img::DenseTaggedImage{T, 3}, i::Int, j::Int, k::Int) where {T} = getindex(img.data, i, j, k)
+@propagate_inbounds Base.getindex(img::DenseTaggedImage{T, N}, i::Vararg{Int, N}) where {T, N} = img.data[i...]
+@propagate_inbounds Base.getindex(img::DenseTaggedImage, i...) = getindex(img.data, i...)
+@propagate_inbounds Base.getindex(img::DenseTaggedImage{T, 3}, i::Int, j::Int, k::Int) where {T} = getindex(img.data, i, j, k)
 
 Base.setindex!(img::DenseTaggedImage, i...) = setindex!(img.data, i...)
 
